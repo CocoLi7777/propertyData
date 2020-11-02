87e49d7d316a27f80d99f07fb477952616b2ae5d
Coco <coco.li7777@gmail.com>
2020-11-02 16:20:40 +1100

    add middleware to check info when newuser register

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const ErrorHandling = require('../utils/errorHandling');
 const User = require('../models/User');
+const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
@@ -43,6 +44,10 @@ exports.createUser = async (req, res, next) => {
       }
     );
   } catch (err) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     console.error(err.message);
     res.status(500).send('Server error');
   }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { createUser } = require('../controllers/users');
+const { check } = require('express-validator/check');
 
 // @route  GET api/users
 // @access public
 
-router.route('/').post(createUser);
-
-router.use('/', express.static('client'));
+router.post(
+  '/',
+  [
+    check('name', 'Please input your name').not().isEmpty(),
+    check('email', 'Please input a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more character'
+    ).isLength({ min: 6 }),
+  ],
+  createUser
+);
 
 module.exports = router;
